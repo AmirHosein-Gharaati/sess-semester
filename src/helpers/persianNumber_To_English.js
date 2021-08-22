a4a4ba308848ea6af6a6d009729f313dd6bf5677
erfanpaslar <paslarerfan1@gmail.com>
2021-08-22 15:52:00 +0430

    persianNumberToEnglish works for just english too

--- src/helpers/persianNumber_To_English.js
+++ src/helpers/persianNumber_To_English.js
@@ -3,7 +3,7 @@ function convertPersianNumToEng(number){
   let res=0
   for(let i=0; i<number.length;i++){
     res*=10
-    res += ((number[i].charCodeAt() - persianNumDifference)-"0".charCodeAt());
+    res += ((number[i].charCodeAt()>256? number[i].charCodeAt()- persianNumDifference:number[i].charCodeAt())-"0".charCodeAt());
   }
   return res;
 }
