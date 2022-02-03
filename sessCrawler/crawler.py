e106fb2c17573ae656d7cb7763c2ef0ea642603a
amir <amirgh1380@gmail.com>
2022-02-03 12:04:41 +0330

    update functions of crawler

--- sessCrawler/crawler.py
+++ sessCrawler/crawler.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 import json
 import re
+
 
 def arabicToPersian(text):
     obj = {"ك":"ک","دِ":"د","بِ":"ب","زِ":"ز","ذِ":"ذ","شِ":"ش","سِ":"س","ى":"ی","ي":"ی","١":"۱","٢":"۲","٣":"۳","٤":"۴","٥":"۵","٦":"۶","٧":"۷","٨":"۸","٩":"۹","٠":"۰","1":"۱","3":"۲","3":"۳","4":"۴","5":"۵","6":"۶","7":"۷","8":"۸","9":"۹","0":"۰"}
@@ -24,7 +26,7 @@ def seperateTimeAndPlace(timeAndDate):
     # timeAndDate
     justDateAndTime = timeAndDate.replace("\n","")
     places = re.findall(r'\(.*?\)',justDateAndTime)
-    justDateAndTime = justDateAndTime.replace(" ","");
+    justDateAndTime = justDateAndTime.replace(" ","")
     justDateAndTime = re.sub(r'\(.*?\)', " ", justDateAndTime).split(" ")[:-1]
 
     thsCourseDaysAndPlace = []
@@ -46,23 +48,23 @@ def get_course_details():
     global Driver
     data = dict()
 
-    data['title'] = arabicToPersian(Driver.find_element_by_id('edName').text)
-    data['vahed'] = Driver.find_element_by_id('edTotalUnit').text
-    data['group'] = Driver.find_element_by_id('edGroup').text
-    data['teacher'] = arabicToPersian(Driver.find_element_by_id('edTch').text)
-    data['gender'] = arabicToPersian(Driver.find_element_by_id('edSex').text)
-    data['unit'] = arabicToPersian(Driver.find_element_by_id('edUnit').text)
-    data['time_in_week'] = arabicToPersian(Driver.find_element_by_id('edTimeInWeek').text)
-    data['time_room'] = arabicToPersian(Driver.find_element_by_id('edTimeRoom').text)
-    data['midterm_date'] = Driver.find_element_by_id('edMidDate').text
-    data['midterm_time'] = Driver.find_element_by_id('edMidTime').text
-    data['capacity'] = arabicToPersian(Driver.find_element_by_id('edCapacity').text)
-    data['id'] = Driver.find_element_by_id('edSrl').text + '^' + data['group']
-    data['final_time'] = Driver.find_element_by_id('edFinalTime').text
-    data['final_date'] = Driver.find_element_by_id('edFinalDate').text
+    data['title'] = arabicToPersian(Driver.find_element(By.ID,  'edName').text)
+    data['vahed'] = Driver.find_element(By.ID,  'edTotalUnit').text
+    data['group'] = Driver.find_element(By.ID,  'edGroup').text
+    data['teacher'] = arabicToPersian(Driver.find_element(By.ID,  'edTch').text)
+    data['gender'] = arabicToPersian(Driver.find_element(By.ID,  'edSex').text)
+    data['unit'] = arabicToPersian(Driver.find_element(By.ID,  'edUnit').text)
+    data['time_in_week'] = arabicToPersian(Driver.find_element(By.ID,  'edTimeInWeek').text)
+    data['time_room'] = arabicToPersian(Driver.find_element(By.ID,  'edTimeRoom').text)
+    data['midterm_date'] = Driver.find_element(By.ID,  'edMidDate').text
+    data['midterm_time'] = Driver.find_element(By.ID,  'edMidTime').text
+    data['capacity'] = arabicToPersian(Driver.find_element(By.ID,  'edCapacity').text)
+    data['id'] = Driver.find_element(By.ID,  'edSrl').text + '^' + data['group']
+    data['final_time'] = Driver.find_element(By.ID,  'edFinalTime').text
+    data['final_date'] = Driver.find_element(By.ID,  'edFinalDate').text
 
     try:
-        splitedTime = Driver.find_element_by_id('edFinalTime').text.replace(" ","").split("-")
+        splitedTime = Driver.find_element(By.ID,  'edFinalTime').text.replace(" ","").split("-")
         splitedTimeStart = splitedTime[0].split(":")
         splitedTimeEnd = splitedTime[1].split(":")
         data['final_time_split'] = {"start_hour":convertIfPersianToEng(splitedTimeStart[0]),"start_minute":convertIfPersianToEng(splitedTimeStart[1]), "end_hour":convertIfPersianToEng(splitedTimeEnd[0]),"end_minute":convertIfPersianToEng(splitedTimeEnd[1])}
@@ -70,7 +72,7 @@ def get_course_details():
         data['final_time_split'] = {"start_hour":0,"start_minute":0, "end_hour":0,"end_minute":0}
 
     try:
-        splitedDate = Driver.find_element_by_id('edFinalDate').text.split("/")
+        splitedDate = Driver.find_element(By.ID,  'edFinalDate').text.split("/")
         data['final_date_split'] = {"d":convertIfPersianToEng(splitedDate[2]),"m":convertIfPersianToEng(splitedDate[1]),"y":convertIfPersianToEng(splitedDate[0])} 
     except:
         data['final_date_split'] = {"d":0,"m":0,"y":0} 
@@ -92,7 +94,7 @@ Driver.get(r'http://sess.shirazu.ac.ir/')
 
 input('Navigate to semester schedule page and press enter to continue ')
 
-select_element = Driver.find_element_by_id('edDepartment')
+select_element = Driver.find_element(By.ID,  'edDepartment')
 select_object = Select(select_element)
 
 all_obj_name = [arabicToPersian(string.text) for string in select_object.options[1:]]
@@ -102,10 +104,10 @@ toCrawlIndexes = [1,9,10,12,18,41,74,77,78,91]
 
 for j in toCrawlIndexes:
     datas = dict()
-    select_element = Driver.find_element_by_id('edDepartment')
+    select_element = Driver.find_element(By.ID,  'edDepartment')
     select_object = Select(select_element)
     select_object.select_by_index(j)
-    Driver.find_element_by_id('edDisplay').click()
+    Driver.find_element(By.ID,  'edDisplay').click()
     print("crawling ",select_element, " ...")
     
 
